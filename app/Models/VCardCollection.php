093cc898b4e2e046db94dfc8b0559d2d9ab513a9
Igor-Cherepanov <cherepanov@batyukovstudio.com>
2021-06-14 23:40:28 +0500

    feat(card)

--- app/Models/VCardCollection.php
+++ app/Models/VCardCollection.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class VCardCollection extends Model
 {
     use HasFactory;
+
+    public $collection = [];
+
+    public function add(){
+
+    }
+
+
+
 }
